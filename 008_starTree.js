a2981c6d5ef2f5d79614f54e70f4404dcb019758
minkyeongJ <jomk2221@naver.com>
2022-05-26 16:49:23 +0900

    별 트리 만들기

---
+++ 008_starTree.js
@@ -0,0 +1,12 @@
+//별찍기
+//    *
+//   ***
+//  *****
+// *******
+//*********
+
+let level = 5;
+
+for (let i = 1; i <= level; i++){
+    console.log(" ".repeat(level - i) + "*".repeat(i * 2 - 1));
+}
